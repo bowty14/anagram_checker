6fce50a8f1a512a5428973a9658b7ce18f2e080d
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-08 08:22:33 -0700

    add test for first spec, expext to fail.

--- Desktop/anagram_checker/spec/anagram_checker_spec.rb
+++ Desktop/anagram_checker/spec/anagram_checker_spec.rb
@@ -1,3 +1,10 @@
 require ('anagram_checker.rb')
 require ('pry')
 require ('rspec')
+
+describe('Anagram#')do
+  it('Will check to see that user input is a word') do
+    word = Anagram.new(345)
+    expect(word.checker()).to(eq("That is not a word."))
+  end
+end
