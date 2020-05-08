5ceae64eb184a0c10b01acbb6f99cb0323596d9d
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-08 09:22:16 -0700

    add test for second spec, expect to fail.

--- Desktop/anagram_checker/spec/anagram_checker_spec.rb
+++ Desktop/anagram_checker/spec/anagram_checker_spec.rb
@@ -7,4 +7,9 @@ describe('Anagram#')do
     word = Anagram.new('ggg', 'ttt')
     expect(word.vowel()).to(eq("This is not a word"))
   end
+
+  it('Will check to see if two words are anagrams') do
+    anagram = Anagram.new('ruby', 'bury')
+    expect(anagram.ana_checker()).to(eq("This is an anagram, nice work!"))
+  end
 end
