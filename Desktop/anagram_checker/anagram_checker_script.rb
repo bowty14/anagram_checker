0478a52a384b9f325fbfb3a1d2882e2faf1839a2
Tyler Bowerman <44910152+bowty14@users.noreply.github.com>
2020-05-08 14:02:24 -0700

    add functionality that prints off results to the user.

--- Desktop/anagram_checker/anagram_checker_script.rb
+++ Desktop/anagram_checker/anagram_checker_script.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env/ ruby
 require('./lib/anagram_checker.rb')
+
+puts "This program is designed to check if two words are either an anagram or an antigram"
+puts " "
+puts "Plese enter your first word"
+word1 = gets.chomp
+puts " "
+puts "Please enter your second word"
+word2 = gets.chomp
+puts " "
+anagram = Anagram.new(word1, word2)
+puts anagram.ana_checker
