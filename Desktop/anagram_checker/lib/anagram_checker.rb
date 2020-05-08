ee9ecfb8a87048dd97d7c218c6eed8bda4824631
Tyler Bowerman <44910152+bowty14@users.noreply.github.com>
2020-05-08 13:54:12 -0700

    refactor code so it works with less code.

--- Desktop/anagram_checker/lib/anagram_checker.rb
+++ Desktop/anagram_checker/lib/anagram_checker.rb
@@ -13,26 +13,11 @@ class Anagram
       return "This is not a word"
     end
   end
-
-  def antigram
-    antigram = true
-    for letters in @word1 do
-      if @word2.include?(letters)
-        antigram = false
-        break
-      end
-    end
-    if antigram
-      return "This is an antigram, nothing matches"
-    end
-    return "It seems as though you may have entered the same word twice, or maybe you entered a palindrome"
-  end
-
   def ana_checker
     if @word1 == @word2
       return "This is an anagram, nice work!"
     end
-    return "These don't seem to match, try again."
+    return "These two words have no matching letters and are therefore antigrams"
   end
 end
 
