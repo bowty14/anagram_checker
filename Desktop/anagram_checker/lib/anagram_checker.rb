edc91c54701c78f15af2c0b4710f787ccb05ef74
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-08 09:16:23 -0700

    pass test.

--- Desktop/anagram_checker/lib/anagram_checker.rb
+++ Desktop/anagram_checker/lib/anagram_checker.rb
@@ -1,15 +1,19 @@
 class Anagram
-  attr_accessor(:word)
-  def initialize(word)
-  @word = word
+  attr_accessor(:word1, :word2)
+
+  def initialize(word1, word2)
+    @word1 = word1.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
+    @word2 = word2.downcase.gsub(/[^a-z0-9]/i, '').split(//).sort
   end
-  def checker
-    if (@word.include(/[A-Z]/i))
-      return true
+
+  def vowel
+    if  @word1.any?{ |x| ["a", "e", "i", "o", "u"].include?(x)} || @word2.any?{ |x| ["a", "e", "i", "o", "u"].include?(x)}
+      return "This is a word."
     else 
-      return false
+      return "This is not a word"
     end
+  end
 end
 
 
-  #  "abc".match?(/[A-Z]/i)
+ 
--- Desktop/anagram_checker/spec/anagram_checker_spec.rb
+++ Desktop/anagram_checker/spec/anagram_checker_spec.rb
@@ -4,7 +4,7 @@ require ('anagram_checker.rb')
 
 describe('Anagram#')do
   it('Will check to see that user input is a word') do
-    word = Anagram.new(345)
-    expect(word.checker()).to(eq("false"))
+    word = Anagram.new('ggg', 'ttt')
+    expect(word.vowel()).to(eq("This is not a word"))
   end
 end
