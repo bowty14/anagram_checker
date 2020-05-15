98ee5ebb92ba5aeeaa1a84ae77b17f9020274bb2
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-14 20:17:59 -0700

    add tests for new methods that are all passing.

--- spec/anagram_checker_spec.rb
+++ spec/anagram_checker_spec.rb
@@ -5,7 +5,7 @@ require ('anagram_checker.rb')
 describe('Anagram#')do
   it('Will output error message if word is not entered') do
     word = Anagram.new('ggg', 'ttt')
-    expect(word.vowel()).to(eq("This is not a word, ignore the prompt below!"))
+    expect(word.vowel()).to(eq("This is not a word."))
   end
 
   it('Will check to see that what is inputted is a word') do
@@ -13,23 +13,38 @@ describe('Anagram#')do
     expect(word.vowel()).to(eq("This is a word."))
   end
 
-  it('Will check to see if two words are anagrams') do
+  it('Will output true if two words are anagrams') do
     anagram = Anagram.new('ruby', 'bury')
-    expect(anagram.ana_checker()).to(eq("This is an anagram, nice work!"))
+    expect(anagram.ana_checker()).to(eq(true))
   end
 
-  it('Will check to see if two word are not anagrams') do
-    antigram = Anagram.new('hi', 'bye')
-    expect(antigram.ana_checker()).to(eq("These two words have no matching letters and are therefore antigrams"))
+  it('Will output false if two word are antigrams') do
+    antigram = Anagram.new('cat', 'bat')
+    expect(antigram.anti_checker()).to(eq(false))
   end
 
-  it('Will check if input is a anagram regardless of capitalization') do 
+  it('Will output true if input is a anagram regardless of capitalization') do 
     cap_anagram = Anagram.new('RubY','BurY')
-    expect(cap_anagram.ana_checker()).to(eq("This is an anagram, nice work!"))
+    expect(cap_anagram.ana_checker()).to(eq(true))
   end
 
-  it('Will check if inputted phrases are anagrams') do 
+  it('Will output true if inputted phrases are anagrams') do 
     phrase = Anagram.new('Stressed angel','Glean Desserts')
-    expect(phrase.ana_checker()).to(eq("This is an anagram, nice work!"))
+    expect(phrase.ana_checker()).to(eq(true))
+  end
+
+  it('Will return "These are anangrams" if ana_checker is true') do 
+    check = Anagram.new('silent','listen')
+    expect(check.word_check()).to(eq("These are anagrams, nice work!"))
+  end
+
+  it('Will return "These words are neither antigrams or anagrams"if anti_checker is true') do 
+    check = Anagram.new('run','base')
+    expect(check.word_check()).to(eq("These words are neither antigrams or anagrams"))
+  end
+
+  it('Will return "These words share similar letters and could be antigrams." if anti_checker is false') do 
+    check = Anagram.new('cat','bat')
+    expect(check.word_check()).to(eq("These words share similar letters and could be antigrams."))
   end
 end
